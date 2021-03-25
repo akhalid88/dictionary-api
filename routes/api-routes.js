eee95c0d86bba1a48e16efbebc19d9a7c2e377af
Muhammad K <akhalid.code@gmail.com>
2021-03-25 12:31:05 -0700

    add additional routes

---
+++ routes/api-routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Dictionary = require("../models/dictionary");
+
+// GET all words
+// /api/words
+router.get("/api/words", (req, res) => {
+	Dictionary.find({})
+		.select('word')
+		.then(dbDictionary => {
+			res.json(dbDictionary);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+// GET meaning of one word
+// /api/word/:word
+router.get("/api/word/:word", (req, res) => {
+	Dictionary.findOne({
+		word: req.params.word
+	})
+		.select('meaning')
+		.then(dbDictionary => {
+			res.json(dbDictionary);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+
+// GET all words that start with a specific letter
+// /api/words/:letter
+router.get("/api/words/:letter", (req, res) => {
+	Dictionary.find({
+		word: { $regex: '^' + req.params.letter, $options: 'i' }
+	})
+		.select('word')
+		.then(dbDictionary => {
+			res.json(dbDictionary);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dictionaryDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
+});
+
+app.use(require("./routes/api-routes.js"));
+
+app.listen(PORT, () => {
+	console.log(`We are listening... ${PORT}!`);
 });
